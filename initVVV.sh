e2754b35cc931e65e6ce5ce66e8ce8645cf881fa
teddyconrick@gmail.com <tconrick>
2020-04-07 12:52:52 +1000

    Deleted initJPCDev; old version of the renamed initVVV

--- initVVV.sh
+++ initVVV.sh
@@ -11,7 +11,6 @@ cp -v default-config.yml VVV/config/default-config.yml
 # This moves to VVV, and goes through the vagrant up process twice;
 # So that VVV can load it's dependencies, then go through a fresh load.
 cd VVV && (yes | vagrant up) 
-&& vagrant up
 # This next section uses the Customfile to insert a line in the Vagrantfile, creating a localResources folder.
 echo "# Customfile insert: Including localResources folder, outside VVV, in JPCDev." >> Customfile
 echo "" >> Customfile
