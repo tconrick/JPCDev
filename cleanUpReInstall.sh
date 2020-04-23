a3b468ee702a081f86ca8175dd8b69067e5b4685
teddyconrick@gmail.com <tconrick>
2020-04-23 10:30:05 +1000

    Finally working again; customfile/synced resources not need for now. Inner vvv repo may not be contained in clone, may have to pull and copy over default-config.yml to config/config.yml

---
+++ cleanUpReInstall.sh
@@ -0,0 +1,4 @@
+cd vagrant-local
+vagrant destroy
+cd ..
+yes | rm -r vagrant-local/ syncedResources/
--- initVVV.sh
+++ initVVV.sh
@@ -10,12 +10,27 @@ git clone -b master git://github.com/Varying-Vagrant-Vagrants/VVV.git
 cp -v default-config.yml VVV/config/default-config.yml
 # This moves to VVV, and goes through the vagrant up process twice;
 # So that VVV can load it's dependencies, then go through a fresh load.
-cd VVV && (yes | vagrant up) 
+cd vagrant-local && (yes | vagrant up) 
 # This next section uses the Customfile to insert a line in the Vagrantfile, creating a localResources folder.
-echo "# Customfile insert: Including localResources folder, outside VVV, in JPCDev." >> Customfile
-echo "" >> Customfile
-parentDir=$(cd ../ && pwd) && echo "config.vm.synced_folder \"$parentDir/localResources\", \"/srv/www\", create: true" >> Customfile
-vagrant reload
+# chmod +x createCustomFile.sh
+# vagrant reload --provision
+cd ..
+# cp -i plugins/wp-migrate-db-pro-1.9.10.zip plugins/wp-migrate-db-pro-cli-1.3.5.zip plugins/wp-migrate-db-pro-media-files-1.4.4.zip plugins/wp-migrate-db-pro-multisite-tools-1.2.6.zip plugins/wp-migrate-db-pro-theme-plugin-files-1.0.5.zip syncedFolder/jpcDev/public_html/wp-content/plugins 
+cp -i plugins/wp-migrate-db-pro-1.9.10.zip plugins/wp-migrate-db-pro-cli-1.3.5.zip plugins/wp-migrate-db-pro-media-files-1.4.4.zip plugins/wp-migrate-db-pro-multisite-tools-1.2.6.zip plugins/wp-migrate-db-pro-theme-plugin-files-1.0.5.zip  vagrant-local/www/jpcDev/public_html/wp-content/plugins 
+# cd syncedFolder/jpcDev/public_html/wp-content/plugins
+cd vagrant-local/www/jpcDev/public_html/wp-content/plugins 
+unzip wp-migrate-db-pro-1.9.10.zip
+unzip wp-migrate-db-pro-cli-1.3.5.zip
+unzip wp-migrate-db-pro-media-files-1.4.4.zip
+unzip wp-migrate-db-pro-multisite-tools-1.2.6.zip
+unzip wp-migrate-db-pro-theme-plugin-files-1.0.5.zip
+rm wp-migrate-db-pro-1.9.10.zip
+rm wp-migrate-db-pro-cli-1.3.5.zip
+rm wp-migrate-db-pro-media-files-1.4.4.zip
+rm wp-migrate-db-pro-multisite-tools-1.2.6.zip
+rm wp-migrate-db-pro-theme-plugin-files-1.0.5.zip
+cd ../../../../../vagrant-local/
+vagrant reload --provision
 
 # More info on making and editing VVV sites via conf file at:
 # https://www.howtoforge.com/setup-a-local-wordpress-development-environment-with-vagrant/
